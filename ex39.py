d77ec5396e13f68fae96f69a01157011f902f6f0
heidy madia <heidy.madia@gmail.com>
2017-01-08 16:46:03 +0700

    modified:   ex39.py 	new file:   ex40.py 	new file:   ex40.txt 	new file:   ex41.py 	new file:   ex41.txt 	new file:   ex42.py

--- ex39.py
+++ ex39.py
@@ -1,2 +1,53 @@
 # create a mapping of state to abbreviation
-states =
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}
+
+# create a basic set of states and some cities in them
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroid',
+    'FL': 'Jacksonville'
+}
+
+# add some more cities
+cities['NY'] = 'New York';
+cities['OR'] = 'Portland';
+
+# print out some cities
+print('-' * 10);
+print('NY State has: ', cities['NY']);
+print('OR State has: ', cities['OR']);
+
+# print some states
+print('-' * 10);
+print("Michigan has: ", cities[states['Michigan']]);
+print("Florida has: ", cities[states['Florida']]);
+
+# do it by using the state then cities dict
+print('-' * 10);
+print('Michigan has: ', cities[states['Michigan']]);
+print('Florida has: ', cities[states['Florida']]);
+
+# print every states appreviation
+print('-' * 10);
+for state, abbrev in states.items():
+    print("%s is abbreviated %s" % (state, abbrev));
+
+# print every city in state
+print('-' * 10);
+for abbrev, city in cities.items():
+    print("%s has the city %s" % (abbrev, city))
+print('-' * 10);
+state = states.get('Texas');
+
+if not state:
+    print("Sorry, no Texas");
+
+# get a city with a default value
+city = cities.get('TX', 'Does Not Exist');
+print("The city for the state 'TX' is: %s" % city);
