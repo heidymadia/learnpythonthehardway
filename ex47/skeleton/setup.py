4cb4da7ceb13555be86622f133b714141b7d0f5b
heidy madia <heidy.madia@gmail.com>
2017-04-24 12:32:04 +0700

    new file:   ex46/skeleton/NAME/__pycache__/__init__.cpython-36.pyc 	new file:   ex46/skeleton/tests/__pycache__/NAME_tests.cpython-36.pyc 	new file:   ex46/skeleton/tests/__pycache__/__init__.cpython-36.pyc 	new file:   ex47/skeleton/ex47/__init__.py 	new file:   ex47/skeleton/ex47/__pycache__/__init__.cpython-34.pyc 	new file:   ex47/skeleton/ex47/__pycache__/__init__.cpython-36.pyc 	new file:   ex47/skeleton/ex47/__pycache__/game.cpython-36.pyc 	new file:   ex47/skeleton/ex47/game.py 	new file:   ex47/skeleton/setup.py 	new file:   ex47/skeleton/tests/__init__.py 	new file:   ex47/skeleton/tests/__pycache__/NAME_tests.cpython-34.pyc 	new file:   ex47/skeleton/tests/__pycache__/NAME_tests.cpython-36.pyc 	new file:   ex47/skeleton/tests/__pycache__/__init__.cpython-34.pyc 	new file:   ex47/skeleton/tests/__pycache__/__init__.cpython-36.pyc 	new file:   ex47/skeleton/tests/__pycache__/ex47_tests.cpython-36.pyc 	new file:   ex47/skeleton/tests/ex47_tests.py 	modified:   ex48/ex48.txt

---
+++ ex47/skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setuptools
+except ImportError:
+    from disutils.core import setuptools
+
+config = {
+    'description': 'My Project',
+    'auther': 'My Name',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'My email.',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['ex47'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config);
