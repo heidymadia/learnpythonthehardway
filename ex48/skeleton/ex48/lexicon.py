a984fa2541e9f0e08c2bfaee76c635ffca34e251
heidy madia <heidy.madia@gmail.com>
2017-04-25 13:27:53 +0700

    modified:   ex48/ex48.txt 	new file:   ex48/skeleton/ex48/__init__.py 	new file:   ex48/skeleton/ex48/__pycache__/__init__.cpython-36.pyc 	new file:   ex48/skeleton/ex48/__pycache__/ex48_tests.cpython-36.pyc 	new file:   ex48/skeleton/ex48/__pycache__/lexicon.cpython-36.pyc 	new file:   ex48/skeleton/ex48/lexicon.py 	new file:   ex48/skeleton/setup.py 	new file:   ex48/skeleton/tests/__init__.py 	new file:   ex48/skeleton/tests/__pycache__/__init__.cpython-36.pyc 	new file:   ex48/skeleton/tests/__pycache__/lexicon_tests.cpython-36.pyc 	new file:   ex48/skeleton/tests/lexicon_tests.py

---
+++ ex48/skeleton/ex48/lexicon.py
@@ -0,0 +1,18 @@
+class Lexicon(object):
+    def __init__(self):
+        pass
+
+    def test_direction(self, direction):
+        pass
+
+    def test_verbs(self, verb):
+        pass
+
+    def test_stops(self, stop):
+        pass
+
+    def test_nouns(self, noun):
+        pass
+
+    def test_numbers(self, number):
+        pass
